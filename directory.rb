b2a9d4813925bf2f3ee3188f831340e49a3114b3
nandhini <nandhini31@gmail.com>
2017-06-25 13:24:42 +0100

    directory.rb

--- directory.rb
+++ directory.rb
@@ -1,82 +1,60 @@
 # first, put all students into an array
-
-def interactive_menu
-	students = []
-	loop do
-	#1.print the menu and ask the user what to do
-		puts "1. Input students"
-		puts "2. Show students"
-		puts "3. Save the list to students.csv"
-		puts "9. Exit"
-		selection = gets.chomp
-		#3.do what user has asked to do
-		case selection
-			when "1"
-			#input the students
-				students = input_students
-			when "2"
-			#print the students
-				print_header
-				print(students)
-				print_footer(students)
-			when "3"
-				save_students
-			when "9"
-			#exit the program
-				exit
-			else
-				puts "I dont know what you meant,please try again"
-		end
-	end
-end
-
+@students  = []
 def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
-	#create an empty array
-	students = []
+
 	#get the first names
 	puts "Please enter the names of the students"
 	name = gets.chomp
-	puts "Please enter the cohort"
-	cohort = gets.chomp
-	puts "Please enter your hobby"
-	hobby = gets.chomp
-	puts "Please enter your nationality"
-	nationality = gets.chomp
-	if name.to_s.empty?
-		name = 'Default name'
-	elsif cohort.to_s.empty?
-		cohort = 'Default'
-	end
 	#while the name is not empty , repeat this code
 	while !name.empty? do
-
-		students << {name: name, cohort: cohort, hobby: hobby,nationality: nationality}
-		if students.count == 1
-			puts puts "Now we have #{students.count} student"
-		else
-			puts "Now we have #{students.count} students"
-		end
-		puts "Please enter the name of the next student"
-		name = gets.chomp
-		puts "Please enter the cohort"
-		cohort =  gets.chomp
-		puts "Please enter your hobby"
-		hobby = gets.chomp
-		puts "Please enter your nationality"
-		nationality = gets.chomp
-
+		@students << {name: name, cohort: :november}
+			 puts "Now we have #{students.count} student"
 	end
-	students
+end
+
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
+	end
+end
+
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "3. Save students list"
+	puts "9.Exit"
 end
 
 
+def show_students
+	print_header
+	print_students_list(students)
+	print_footer(students)
+end
+
+def process_students
+	case selection
+		when "1"
+			input_students
+		when "2"
+			show_students
+		when "9"
+			exit
+		else
+			puts "I don't know what you mean try again"
+		end
+end
+
+
+
 def save_students
-	#open the file for writing
+#open the file for writing
 	file = File.open("students.csv","w")
 	#iterate over the array of students
-	@students.each do  |student|
+	@students.each do |student|
 		student_each = [student[:name],student[:cohort]]
 		csv_line = student_data.join(",")
 		file.puts csv_line
@@ -85,42 +63,23 @@ def save_students
 end
 
 
+def load_students
+	file = File.open("students.csv", "r")
+	file.readlines.each do |line|
+	name,cohort = line.chomp.split(',')
+			@students << {name: name,cohort: cohort.to_sym}
+	end
+	file.close
+end
+
 def print_header
 	puts "The students of Villians Academy".center(50)
 	puts "-----------".center(50)
 end
 
-def sort_by_cohort(students)
-	cohort_array = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
-	student_by_cohort = students.sort_by {|student| cohort_array.index(student[:cohort])}
-
-end
-
-def print(students)
-	if students.count <= 0
-		return
-	else
-		i = 0
-		while i < students.count
-			puts "#{students[i][:name]} (#{students[i][:cohort]} cohort) #{students[i][:hobby]} #{students[i][:nationality]} ".center(50)
-			i = i+1
-		end
-	end
-end
-
-def print_by_letter(students,initial)
-	students.each_with_index do |student,index|
-		if student[:name][0] == initial
-			puts puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort) "
-		end
-	end
-end
-
-def print_conditions(students,initial,size)
-	students.each_with_index do |student,index|
-		if student[:name].length <= size && student[:name][0] == initial.downcase
-			 puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort) ".center(50)
-		end
+def print_students_list
+	@students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
@@ -128,7 +87,3 @@ end
 def print_footer(names)
 	puts "Overall, we have #{names.count} great students"
 end
-
-
-
-interactive_menu
